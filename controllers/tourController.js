75fe29084649a17a763b5d8c49ec664ecac97fba
dingtongwang0 <dingtongwang@outlook.com>
2021-08-23 02:21:43 -0400

    add controller getTourStats and router '/tours/tour-stats'

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -131,3 +131,52 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+exports.getTourStats = async (req, res) => {
+  const test = await Tour.aggregate([
+    {
+      $match: { ratingsAverage: { $gte: 4.5 } },
+    },
+    {
+      $group: {
+        _id: null,
+        avgRating: { $avg: '$ratingsAverage' },
+        avgPrice: { $avg: '$price' },
+        minPrice: { $min: '$price' },
+        maxPrice: { $max: '$price' },
+      },
+    },
+  ]);
+
+  console.log(test);
+
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          num: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      { $sort: { avgPrice: 1 } },
+      // { $match: { _id: { $ne: 'EASY' } } },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: { stats },
+    });
+  } catch (err) {
+    res.status(404).json({
+      stauts: 'failed',
+      message: err,
+    });
+  }
+};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -14,6 +14,8 @@ router
   .get(tourController.getAllTours)
   .post(tourController.createTour);
 
+router.route('/tour-stats').get(tourController.getTourStats);
+
 router
   .route('/:id')
   .get(tourController.getTour)
