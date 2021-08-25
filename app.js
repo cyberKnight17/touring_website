20f74686e2e2a973526bbe546e2bd7c199e5a4dd
dingtongwang0 <dingtongwang@outlook.com>
2021-08-25 18:40:45 -0400

    add Routes to handle undefined routes

--- app.js
+++ app.js
@@ -24,7 +24,15 @@ app.use((req, res, next) => {
   next();
 });
 
+// 3 ) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'failed',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
