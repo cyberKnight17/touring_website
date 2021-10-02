e0f2043f462be48c375dfeb53f0c3fe26d591e78
dingtongwang0 <dingtongwang@outlook.com>
2021-10-02 00:00:46 -0400

    add development and production error handlers

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,37 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    stack: err.stack,
   });
+};
+
+const sendErrorProd = (err, res) => {
+  // if the error is operational error, send the message to the client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // if the error is programming error or other error, don't leak error details
+  } else {
+    // 1) log error
+    console.log('ERROR 💥', err);
+
+    // 2) send generic message
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: 'Something wrong happened. Please refresh.',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.stauts = err.status || 'error';
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
