9a06701298b5fee8322d916990ed66dfac348fb7
dingtongwang0 <dingtongwang@outlook.com>
2021-10-07 16:10:25 -0400

    add mongdb error handler

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-const { RSA_NO_PADDING } = require('constants');
 const express = require('express');
-
+const morgan = require('morgan');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-const morgan = require('morgan');
-
 const app = express();
 
 // 1. Middlewares
-
-// console.log(process.env.NODE_ENV);
-
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 app.use(express.json());
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,15 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const { message } = err;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -13,10 +25,12 @@ const sendErrorProd = (err, res) => {
       status: err.status,
       message: err.message,
     });
-    // if the error is programming error or other error, don't leak error details
-  } else {
+  }
+
+  // if the error is programming error or other error, don't leak error details
+  else {
     // 1) log error
-    console.log('ERROR 💥', err);
+    // console.log('ERROR 💥', err);
 
     // 2) send generic message
     res.status(err.statusCode).json({
@@ -29,9 +43,15 @@ const sendErrorProd = (err, res) => {
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.stauts = err.status || 'error';
+
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
+    if (err.name === 'CastError') {
+      err = handleCastErrorDB(err);
+    } else if (err.code === 11000) {
+      err = handleDuplicateFieldsDB(err);
+    }
     sendErrorProd(err, res);
   }
 };
